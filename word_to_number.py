d0fe85c06e026c78ebf41d7b305ed9cd43121d68
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-03-29 12:03:37 +1100

    added basic "word to number" function for press releases, e.g. to allow for parsing "twenty two cases" to "22 cases"

--- word_to_number.py
+++ word_to_number.py
@@ -1,5 +1,42 @@
+import re
+from re import IGNORECASE
 
-def word_to_number():
-    # TODO!
-    raise NotImplementedError()
 
+def word_to_number(s):
+    # TODO: Convert a/an as well!!! =====================================
+    #       "Both"
+    #       hyphenated
+    #       "first"/"second" etc
+
+    less_than_20 = [
+        'zero', 'one', 'two', 'three', 'four', 'five',
+        'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen',
+        'fifteen', 'sixteen', 'seventeen', 'eighteen',
+        'nineteen'
+    ]
+    tens = [
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty',
+        'seventy', 'eighty', 'ninety'
+    ]
+
+    for x, ten in enumerate(tens):
+        ten = ten.lower()
+
+        for y, num in enumerate(less_than_20):
+            num = num.lower()
+            s = re.sub(r'\b'+ten+' '+num+r'\b',
+                       str((10*(x+2))+y), s,
+                       flags=IGNORECASE)
+        s = re.sub(r'\b%s\b' % str(10*(x+2)), str(x), s,
+                   flags=IGNORECASE)
+
+    for x, num in enumerate(less_than_20):
+        num = num.lower()
+        s = re.sub(r'\b%s\b' % num, str(x), s,
+                   flags=IGNORECASE)
+    return s
+
+
+if __name__ == '__main__':
+    print(word_to_number('dsajkldjsa gklrewfjkd twenty five four five'))
