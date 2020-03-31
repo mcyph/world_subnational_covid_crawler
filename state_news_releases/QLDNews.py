72c4cb41657878c31db91697d745afe7d90013ca
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-03-31 20:45:51 +1100

    Queensland total cases tested over time is working now for time series data

--- state_news_releases/QLDNews.py
+++ state_news_releases/QLDNews.py
@@ -1,6 +1,10 @@
 from pyquery import PyQuery as pq
 from re import compile, MULTILINE, DOTALL
+
 from covid_19_au_grab.state_news_releases.StateNewsBase import StateNewsBase
+from covid_19_au_grab.state_news_releases.constants import DT_CASES_TESTED
+from covid_19_au_grab.state_news_releases.data_containers.DataPoint import \
+    DataPoint
 
 
 class QLDNews(StateNewsBase):
@@ -8,10 +12,32 @@ class QLDNews(StateNewsBase):
     LISTING_URL = 'https://www.qld.gov.au/health/conditions/' \
                   'health-alerts/coronavirus-covid-19/current-status'
     LISTING_HREF_SELECTOR = '#qg-primary-content a'
+    STATS_BY_REGION_URL = 'https://www.qld.gov.au/health/conditions/' \
+                          'health-alerts/coronavirus-covid-19/' \
+                          'current-status/' \
+                          'current-status-and-contact-tracing-alerts'
 
-    # Updates for regions is at
-    # https://www.health.qld.gov.au/news-events/doh-media-releases/releases/queensland-novel-coronavirus-covid-19-update9
-    #
+    def get_data(self):
+        #StateNewsBase.get_data(self)
+
+        # Make sure the latest is in the cache!
+        self.current_status_ua.get_url_data(
+            self.STATS_BY_REGION_URL
+        )
+
+        output = []
+        for period in self.current_status_ua.iter_periods():
+            for subperiod_id, subdir in self.current_status_ua.iter_paths_for_period(period):
+                path = self.current_status_ua.get_path(subdir)
+
+                with open(path, 'r', encoding='utf-8', errors='ignore') as f:
+                    html = f.read()
+                    total_cases_tested = self._get_total_cases_tested(
+                        self.STATS_BY_REGION_URL, html
+                    )
+                    if total_cases_tested is not None:
+                        output.append(total_cases_tested)
+        return output
 
     def _infer_missing_info(self, dates_dict):
         # TODO: QLD health now only provides a tally, but previously
@@ -20,7 +46,10 @@ class QLDNews(StateNewsBase):
         pass
 
     def _get_date(self, href, html):
-        'div#content div h2'
+        return self._extract_date_using_format(
+            # e.g. 24 March 2020
+            pq(html)('div#content div h2').text().strip()
+        )
 
     #============================================================#
     #                      General Totals                        #
@@ -30,10 +59,7 @@ class QLDNews(StateNewsBase):
         return self._extract_number_using_regex(
             compile('state total to ([0-9,]+)'),
             html
-        )
-
-    def _get_total_cases_tested(self, href, html):
-        return self._extract_number_using_regex(
+        ) or self._extract_number_using_regex(
             compile(
                 # Total number changed from being enclosed in a <strong>
                 # tag to a <b> tag, so changed to be as broad as NSW
@@ -45,6 +71,56 @@ class QLDNews(StateNewsBase):
             ),
             html
         )
+
+    def _get_total_new_cases(self, href, html):
+        return self._extract_number_using_regex(
+            '[0-9,] new cases',
+            html
+        )
+
+    def _get_total_cases_tested(self, href, html):
+        # NOTE: This is actually a different page to the press releases!
+        #  I needed to get some of these from web.archive.org.
+        #  Some of the stats may be a day or more old,
+        #  so will need to add the date of the stat as well(!)
+
+        # Find the start of the # samples tested table
+        th_regex = compile(
+            '<th id="table[^"]+">[^<]*?As at ([^<]+)[^<]*?</th>[^<]*'
+            '<th id="table[^"]+">[^<]*?Samples tested[^<]*?</th>',
+            DOTALL | MULTILINE
+        )
+        match = th_regex.search(html)
+        if not match:
+            print("NOT INITIAL MATCH!")
+            return None  # WARNING!!!
+
+        # Get the date - it's in format "30 March 2020"
+        date_updated = self._extract_date_using_format(
+            match.group(1).strip()
+        )
+        slice_from = match.end(1)  # CHECK ME!
+        html = html[slice_from:]
+
+        # Get the # samples total
+        value = self._extract_number_using_regex(
+            compile(
+                # Total number changed from being enclosed in a <strong>
+                # tag to a <b> tag, so changed to be as broad as NSW
+                # <strong>Total</strong></td>
+                # <td headers="table59454r1c2"><b>37,334‬</b></td>
+                r'<td[^>]*?>(?:<[^</>]+>)?Total(?:</[^<>]+>)?</td>'
+                r'[^<]*?<td[^>]*?>.*?([0-9,]+).*?</td>',
+                MULTILINE | DOTALL
+            ), html,
+            date_updated=date_updated,
+            datatype=DT_CASES_TESTED,
+            source_url=href,
+        )
+        if not value:
+            print("NOT SECOND MATCH!")
+            return None  # WARNING!
+        return value
 
     #============================================================#
     #                  Male/Female Breakdown                     #
@@ -86,7 +162,7 @@ class QLDNews(StateNewsBase):
 
         return regions
 
-    def _get_total_new_cases_by_region(self, href, html):
+    def _get_new_cases_by_region(self, href, html):
         # TODO: QLD only provided new cases for a few days
         # it might pay to do a tally of some kind!
         ' New confirmed cases'
@@ -126,3 +202,9 @@ class QLDNews(StateNewsBase):
 
     def _get_total_source_of_infection(self, url, html):
         pass
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    qn = QLDNews()
+    pprint(qn.get_data())
