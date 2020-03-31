cf03c82fb390b222eb58f74065d643284bb55a58
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-04-01 09:23:47 +1100

    act grabber now works for test numbers

--- state_news_releases/ACTNews.py
+++ state_news_releases/ACTNews.py
@@ -1,42 +1,28 @@
 from re import compile
 from pyquery import PyQuery as pq
-from covid_19_au_grab.state_news_releases.StateNewsBase import StateNewsBase
+
+from covid_19_au_grab.state_news_releases.StateNewsBase import \
+    StateNewsBase
+from covid_19_au_grab.state_news_releases.constants import \
+    DT_CASES_TESTED, DT_CASES
+from covid_19_au_grab.state_news_releases.data_containers.DataPoint import \
+    DataPoint
 
 
 class ACTNews(StateNewsBase):
-    # FIXME: THIS GRABBER IS ONLY A SINGLE PAGE - IT REALLY NEEDS TO BREAK IT UP!!! ==================================
-
     STATE_NAME = 'act'
-    LISTING_URL = []#'https://www.health.act.gov.au/about-our-health-system/'  \
-                  #'novel-coronavirus-covid-19'
-    LISTING_HREF_SELECTOR = '.latestnewsinner a'
-
-    def get_data(self):
-        def get_until(elm, selector):
-            L = []
-            for i in elm:
-                if pq(i).is_(selector):
-                    break
-                L.append(i)
-            return pq(L)
-
-        url = 'https://www.health.act.gov.au/about-our-health-system/' \
-              'novel-coronavirus-covid-19/latest-news'
-        html = pq(url=url)
-
-        for h3 in html('.timelinemain h3'):
-            elms_in_h3 = get_until(h3.next_all(), 'h3')
-
-            date = self._get_date(
-                FIXME, pq(h3).html() + pq(elms_in_h3)
-            )
-
-            FIXME
+    LISTING_URL = (
+        # There's only 2 pages for now, so will hardcode
+        # MUST BE UPDATED!!! ===========================================================================================
+        'https://www.covid19.act.gov.au/topics?queries_topic_query=0002',
+        'https://www.covid19.act.gov.au/topics?queries_topic_query=0002'
+            '&result_1504801_result_page=2',
+    )
+    LISTING_HREF_SELECTOR = '.card .card-content .card--title a'
 
     def _get_date(self, href, html):
-        text = pq(html)('h3').text().strip()
-        date_str = text.partition(' ')[-1].split('-')[0].strip()
-        return self._extract_date_using_format(date_str, '%d %B')
+        date = pq(html)('.article-body--date p').text().strip()
+        return self._extract_date_using_format(date)
 
     #============================================================#
     #                      General Totals                        #
@@ -45,24 +31,45 @@ class ACTNews(StateNewsBase):
     def _get_total_cases(self, href, html):
         return self._extract_number_using_regex(
             compile(r'total to ([0-9,]+)'),
-            html
+            html,
+            source_url=href,
+            datatype=DT_CASES,
+            date_updated=self._get_date(href, html)
         ) or self._extract_number_using_regex(
             compile(r'confirmed cases in the ACT is now ([0-9,]+)'),
-            html
+            html,
+            source_url=href,
+            datatype=DT_CASES,
+            date_updated=self._get_date(href, html)
         )
 
     def _get_total_cases_tested(self, href, html):
-        neg = self._extract_number_using_regex(
+        neg_cases = self._extract_number_using_regex(
             compile('There have been ([0-9,]+) negative'),
-            html
+            html,
+            source_url=href,
+            datatype=DT_CASES_TESTED,
+            date_updated=self._get_date(href, html)
         ) or self._extract_number_using_regex(
             compile(r'tested negative is now ([0-9,]+)'),
-            html
+            html,
+            source_url=href,
+            datatype=DT_CASES_TESTED,
+            date_updated=self._get_date(href, html)
         )
-        pos = self._get_total_cases(href, html)
+        pos_cases = self._get_total_cases(href, html)
 
-        if neg is not None and pos is not None:
-            return neg + pos
+        if neg_cases is not None and pos_cases is not None:
+            return DataPoint(
+                datatype=neg_cases.datatype,
+                value=neg_cases.value + pos_cases.value,
+                date_updated=neg_cases.date_updated,
+                source_url=neg_cases.source_url,
+                text_match=(
+                    neg_cases.text_match,
+                    pos_cases.text_match
+                )
+            )
         return None
 
     def _get_total_new_cases(self, href, html):
@@ -103,3 +110,8 @@ class ACTNews(StateNewsBase):
     def _get_total_source_of_infection(self, url, html):
         pass
 
+
+if __name__ == '__main__':
+    from pprint import pprint
+    an = ACTNews()
+    pprint(an.get_data())
