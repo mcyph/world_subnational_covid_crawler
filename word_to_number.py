fa09bab0c27fa7a20916924d3019e8fe42264b3e
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-04-01 23:08:38 +1100

    basic support for for a/an/both, hyphenated words and ordinals in word_to_number.py

--- word_to_number.py
+++ word_to_number.py
@@ -3,11 +3,6 @@ from re import IGNORECASE
 
 
 def word_to_number(s):
-    # TODO: Convert a/an as well!!! =====================================
-    #       "Both"
-    #       hyphenated
-    #       "first"/"second" etc
-
     less_than_20 = [
         'zero', 'one', 'two', 'three', 'four', 'five',
         'six', 'seven', 'eight', 'nine', 'ten',
@@ -19,6 +14,15 @@ def word_to_number(s):
         'twenty', 'thirty', 'forty', 'fifty', 'sixty',
         'seventy', 'eighty', 'ninety'
     ]
+    ordinals = [
+        'first', 'second', 'third', 'fourth', 'fifth',
+        'sixth', 'seventh', 'eighth', 'ninth', 'tenth'
+    ]
+
+    s = re.sub(r'\b(a|an)\b', '1', s,
+               flags=IGNORECASE)
+    s = re.sub(r'\bboth\b', '2', s,
+               flags=IGNORECASE)
 
     for x, ten in enumerate(tens):
         ten = ten.lower()
@@ -28,15 +32,30 @@ def word_to_number(s):
             s = re.sub(r'\b'+ten+' '+num+r'\b',
                        str((10*(x+2))+y), s,
                        flags=IGNORECASE)
-        s = re.sub(r'\b%s\b' % str(10*(x+2)), str(x), s,
+            s = re.sub(r'\b'+ten+'-'+num+r'\b',
+                       str((10*(x+2))+y), s,
+                       flags=IGNORECASE)
+        s = re.sub(r'\b%s\b' % ten, str(10*(x+2)), s,
                    flags=IGNORECASE)
 
     for x, num in enumerate(less_than_20):
         num = num.lower()
         s = re.sub(r'\b%s\b' % num, str(x), s,
                    flags=IGNORECASE)
+
+    # Convert "first case" etc to "1 case"
+    for x, ordinal in enumerate(ordinals):
+        ordinal = ordinal.lower()
+        s = re.sub(r'\b%s\b' % ordinal, '1', s,
+                   flags=IGNORECASE)
+
     return s
 
 
 if __name__ == '__main__':
     print(word_to_number('dsajkldjsa gklrewfjkd twenty five four five'))
+    print(word_to_number('20 March 2020'))
+    print(word_to_number('first case'))
+    print(word_to_number('The new cases include 18 males and 12 females, aged between 21 and 80.'))
+    print(word_to_number('Both cases are men. One is from Southern Tasmania and one is from Northern Tasmania. One is aged in their 20s and the other is in their 70s.'))
+    print(word_to_number('Tasmania has today confirmed six more cases of coronavirus'))
