6a0497a2d648e918242305e74a8323218345048f
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-04-01 23:06:33 +1100

    added allowing for using the period argument in URLArchiver as a callable, so the date/cache path can be determined once the URL has downloaded

--- URLArchiver.py
+++ URLArchiver.py
@@ -2,8 +2,8 @@ import re
 import datetime
 import unicodedata
 from os import listdir, makedirs
-from os.path import isfile, expanduser
-from urllib.request import urlretrieve
+from os.path import isfile, expanduser, exists, dirname
+from urllib.request import urlretrieve, urlopen
 
 
 BASE_PATH = expanduser('~/dev/covid_19_data')
@@ -33,42 +33,111 @@ class URLArchiver:
         path = f'{dir_}/{fnam}'
         return path
 
-    def get_url_data(self, url, period=None):
+    def get_url_data(self, url, period=None, cache=True):
         """
-        Note that if url is `None` here, it will
+        Download data from `url`, optionally
+
+        :param url: the url to download
+        :param period: either "YYYY_MM_DD" period-formatted string,
+                       a function which returns a "YYYY_MM_DD" string
+                           based on the url and the data at `url`,
+                       or `None`
+        :param cache: whether to use a cached copy.
+                      when `False` and `period` is a function,
+                          overwrite the cache item
+                      when `False` and `period` is a string,
+                          don't overwrite, but use a new subperiod id
+                      when `True` and `period` is a function,
+                          FIXME
+                      when `True` and `period` is a string,
+                          FIXME
         """
+
         # I'm using YYYY_MM_DD-ID here, so
-        # that it sorts in date then ID order
+        #   that it binary sorts in
+        #   year-month-day, then ID order
         if period is None:
             t = datetime.datetime.now()
             period = t.strftime('%Y_%m_%d')
         elif isinstance(period, datetime.datetime):
             period = period.strftime('%Y_%m_%d')
 
-        subperiod_id = self._get_subperiod_id_for_url(period, url)
-        dir_ = f'{self.base_dir}/{period}-{subperiod_id}'
-
-        try:
-            makedirs(dir_)
-        except OSError:
-            pass
-
-        try:
-            fnam = self._get_first_fnam_in_dir(dir_)
-        except FileNotFoundError:
-            # Download if a cached version
-            # for this URL can't be found
+        if callable(period):
+            # Provide support for getting the period
+            # based on the content located at the url
+            # by providing the content to the
+            # user-supplied "obtain period" function.
             fnam = self._escape_url(url)
-            urlretrieve(url, f'{dir_}/{fnam}')
+            found_path = None
 
-        with open(f'{dir_}/{fnam}', 'r',
-                  encoding='utf-8',
-                  errors='replace') as f:
-            return f.read()
+            for subdir in listdir(self.base_dir):
+                # The period(/date) of the content can't be
+                # figured out until it's been downloaded.
+                # If there's any file which corresponds to `url`
+                # in any of the period subdirs, then we'll use it.
+                check_path = f'{self.base_dir}/{subdir}/{fnam}'
+                if exists(check_path) and cache:
+                    with open(check_path, 'r',
+                              encoding='utf-8',
+                              errors='replace') as f:
+                        return f.read()
+                elif exists(check_path):
+                    # If cache is disabled, overwrite regardless!
+                    found_path = check_path
+                    break
+
+            with urlopen(url) as f:
+                data = f.read().decode(
+                    'utf-8', errors='replace'
+                )
+
+            if found_path:
+                dir_ = dirname(found_path)
+            else:
+                period = period(url, data)
+                subperiod_id = self._get_subperiod_id_for_url(period, url)
+                dir_ = f'{self.base_dir}/{period}-{subperiod_id}'
+
+            try:
+                makedirs(dir_)
+            except OSError:
+                pass
+
+            with open(f'{dir_}/{fnam}', 'w',
+                      encoding='utf-8',
+                      errors='replace') as f:
+                f.write(data)
+            return data
+
+        else:
+            subperiod_id = self._get_subperiod_id_for_url(period, url)
+            dir_ = f'{self.base_dir}/{period}-{subperiod_id}'
+
+            try:
+                makedirs(dir_)
+            except OSError:
+                pass
+
+            try:
+                if not cache:
+                    # Always download if cache is disabled!
+                    raise FileNotFoundError()
+                fnam = self._get_first_fnam_in_dir(dir_)
+            except FileNotFoundError:
+                # Download if a cached version
+                # for this URL can't be found
+                fnam = self._escape_url(url)
+                urlretrieve(url, f'{dir_}/{fnam}')
+
+            with open(f'{dir_}/{fnam}', 'r',
+                      encoding='utf-8',
+                      errors='replace') as f:
+                return f.read()
 
     def iter_periods(self, newest_first=True):
         """
-
+        Iterate through all the periods without the subperiod ID
+        i.e. in YYYY_MM_DD format
         """
         for dir_ in sorted(listdir(self.base_dir),
                            reverse=newest_first):
@@ -77,7 +146,9 @@ class URLArchiver:
 
     def iter_paths_for_period(self, period):
         """
-
+        Iterate through subperiods within a period.
+        Yields the subperiod ID (which is 1+) and the subdirectory
+        (which is in the [YYYY]_[MM]_[DD]-[subperiod ID] format)
         """
         for dir_ in listdir(f"{self.base_dir}"):
             if dir_.split('-')[0] == period:
