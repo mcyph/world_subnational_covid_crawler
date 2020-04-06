b22ffc7f5d69ed0f30c9198a2df8acaa07ee0529
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-04-06 16:56:41 +1000

    fixed ACT source of infection and male/female breakdown

--- state_news_releases/ACTNews.py
+++ state_news_releases/ACTNews.py
@@ -47,12 +47,14 @@ class ACTNews(StateNewsBase):
     #============================================================#
 
     def _get_total_cases(self, href, html):
+        # TODO: UPDATE ME and make me work with confirmed case info!!! ====================================
         c_html = word_to_number(html)
 
         return self._extract_number_using_regex(
             (
                 compile(r'total to ([0-9,]+)'),
-                compile(r'confirmed cases [^0-9.]+([0-9,]+)')
+                compile(r'confirmed cases [^0-9.]+([0-9,]+)'),
+                compile(r'total remains at (?:<strong>)?([0-9,]+)(?:</strong>)?')
             ),
             c_html,
             source_url=href,
@@ -61,6 +63,7 @@ class ACTNews(StateNewsBase):
         )
 
     def _get_total_cases_tested(self, href, html):
+        # TODO: UPDATE ME and make me work with confirmed case info!!! ====================================
         c_html = word_to_number(html)
 
         neg_cases = self._extract_number_using_regex(
@@ -95,7 +98,7 @@ class ACTNews(StateNewsBase):
 
         return self._extract_number_using_regex(
             compile(
-                r'([0-9,]+)\)? new confirmed'
+                r'([0-9,]+)\)? new (?:confirmed|cases?)'
             ),
             c_html,
             source_url=href,
@@ -182,19 +185,22 @@ class ACTNews(StateNewsBase):
             r.append(female)
         return r
 
-    @singledaystat
+    #@singledaystat
     def _get_total_male_female_breakdown(self, href, html):
         # https://www.covid19.act.gov.au/updates/confirmed-case-information
         # Only at stats by region URL!!!
         # Well, they had it for one day at the reports, but..)
 
+        # ADDITION: This looks to only support the reports(?)
+        # should the stats by region URL be supported(!?) ============================================================
+
         c_html = word_to_number(html)
 
         male = self._extract_number_using_regex(
             compile(
-                '<p><strong>By sex</strong></p></td></tr>'
-                '<tr><td><p>Male</p></td><td><p>([0-9,]+)</p></td></tr>'
-                '<tr><td><p>Female</p></td><td><p>(?:[0-9,]+)</p>',
+                '<p[^>]*><strong>By sex</strong></p></td></tr>'
+                '<tr[^>]*><td[^>]*><p[^>]*>Male</p></td><td[^>]*><p[^>]*>([0-9,]+)</p></td></tr>'
+                '<tr[^>]*><td[^>]*><p[^>]*>Female</p></td><td[^>]*><p[^>]*>(?:[0-9,]+)</p>',
                 IGNORECASE
             ),
             c_html,
@@ -204,9 +210,9 @@ class ACTNews(StateNewsBase):
         )
         female = self._extract_number_using_regex(
             compile(
-                '<p><strong>By sex</strong></p></td></tr>'
-                '<tr><td><p>Male</p></td><td><p>(?:[0-9,]+)</p></td></tr>'
-                '<tr><td><p>Female</p></td><td><p>([0-9,]+)</p>',
+                '<p[^>]*><strong[^>]*>By sex</strong></p></td></tr>'
+                '<tr[^>]*><td[^>]*><p[^>]*>Male</p></td><td[^>]*><p[^>]*>(?:[0-9,]+)</p></td></tr>'
+                '<tr[^>]*><td[^>]*><p[^>]*>Female</p></td><td[^>]*><p[^>]*>([0-9,]+)</p>',
                 IGNORECASE
             ),
             c_html,
@@ -237,41 +243,49 @@ class ACTNews(StateNewsBase):
     def _get_new_source_of_infection(self, url, html):
         pass
 
-    @singledaystat
+    #@singledaystat
     def _get_total_source_of_infection(self, url, html):
-        # only source is at
+        # NOTE: there are also stats at
         # https://www.covid19.act.gov.au/updates/confirmed-case-information
+        # but they're in a different format -
+        # not sure it's worth supporting them
 
-        re_soi = compile(
-            r'<strong>By likely source of infection</strong></p></td>'
-                r'<td><p><strong> Count</strong></p></td></tr>'
-            r'<tr><td><p>Overseas acquired</p></td>'
-                r'<td><p>(?P<Overseas_Acquired>[0-9,]+)</p></td></tr>'
-            r'<tr><td><p>Interstate acquired</p></td>'
-                r'<td><p>(?P<Interstate_Acquired>[0-9,]+)</p></td></tr>'
-            r'<tr><td><p>Contact of a confirmed ACT case</p></td>'
-                r'<td><p>(?P<Contact_of_a_Confirmed_ACT_Case>[0-9,]+)</p></td></tr>'
-            r'<tr><td><p>Unknown / local transmission</p></td>'
-                r'<td><p>(?P<Unknown_or_Local_Transmission>[0-9,]+)</p></td></tr>',
-            IGNORECASE
-        )
+        r = []
+        for re_text in (
+            r'<tr[^>]*><td[^>]*><p[^>]*>Overseas acquired</p></td>'
+                r'<td[^>]*><p[^>]*>(?P<Overseas_acquired>[0-9,]+)</p></td></tr>',
+            # Cruise ship-acquired was only added around 6 April
+            r'<tr[^>]*><td[^>]*><p[^>]*>Cruise ship acquired</p></td>'
+                r'<td[^>]*><p[^>]*>(?P<Cruise_ship_acquired>[0-9,]+) of the [0-9,]+</p></td></tr>',
+            r'<tr[^>]*><td[^>]*><p[^>]*>Interstate acquired</p></td>'
+                r'<td[^>]*><p[^>]*>(?P<Interstate_acquired>[0-9,]+)</p></td></tr>',
+            r'<tr[^>]*><td[^>]*><p[^>]*>Contact of a confirmed ACT case</p></td>'
+                r'<td[^>]*><p[^>]*>(?P<Contact_of_a_confirmed_ACT_case>[0-9,]+)</p></td></tr>',
+            r'<tr[^>]*><td[^>]*><p[^>]*>Unknown / local transmission</p></td>'
+                r'<td[^>]*><p[^>]*>(?P<Unknown_or_local_transmission>[0-9,]+)</p></td></tr>',
+            r'<tr[^>]*><td[^>]*><p[^>]*>Under investigation</p></td>'
+                r'<td[^>]*><p[^>]*>(?P<Under_investigation>[0-9,]+)</p></td></tr>'
+        ):
+            re_soi = compile(re_text, IGNORECASE)
 
-        match = re_soi.match(html)
-        if match:
-            r = []
-            gd = match.groupdict()
+            match = re_soi.search(html)
+            if match:
+                gd = match.groupdict()
 
-            for k, v in gd.items():
-                r.append(DataPoint(
-                    name=k.replace('_', ' '),
-                    datatype=DT_SOURCE_OF_INFECTION,
-                    value=int(v.replace(',', '')),
-                    date_updated=self._get_date(url, html),
-                    source_url=url,
-                    text_match=None  # HACK!
-                ))
-            return r
-        return None
+                for k, v in gd.items():
+                    if v is None:
+                        continue
+
+                    r.append(DataPoint(
+                        name=k.replace('_', ' '),
+                        datatype=DT_SOURCE_OF_INFECTION,
+                        value=int(v.replace(',', '')),
+                        date_updated=self._get_date(url, html),
+                        source_url=url,
+                        text_match=None  # HACK!
+                    ))
+
+        return r or None
 
     #============================================================#
     #               Deaths/Hospitalized/Recovered                #
