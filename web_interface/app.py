d9f5f52a817933f35c8373717b5151bfee779379
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-04-13 15:55:09 +1000

    added statistics report page to the web app

--- web_interface/app.py
+++ web_interface/app.py
@@ -1,4 +1,5 @@
 import csv
+from pytz import timezone
 from os import listdir
 from os.path import getctime, expanduser
 import cherrypy
@@ -41,27 +42,59 @@ class App(object):
             for row in reader:
                 out.append(row)
 
-        def sortable_date(i):
-            dd, mm, yyyy = i.split('/')
-            return str(9999-int(yyyy)) + '_' + str(99-int(mm)) + '_' + str(99-int(dd))
-
-        out.sort(key=lambda x: (
-            sortable_date(x['date_updated']),
-            x['state_name'],
-            x['datatype'],
-            x['name']
-        ))
-
+        out.sort(key=self.__date_updated_sort_key)
         return out
 
     def __get_revisions(self):
         # Return [[rev_date, rev_subid, rev_time], ...]
         r = []
+
         for fnam in listdir(OUTPUT_DIR):
             rev_date, rev_subid = fnam[:-4].split('-')
             rev_time = getctime(f'{OUTPUT_DIR}/{fnam}')
-            r.append((rev_date, rev_subid, rev_time))
+            dt = str(datetime.datetime.fromtimestamp(rev_time) \
+                .astimezone(timezone('Australia/Melbourne'))).split('.')[0]
+            r.append((rev_date, rev_subid, dt))
+
+        r.sort(reverse=True)
         return r
+
+    def __get_revision_time_string(self, period, subperiod_id):
+        rev_time = getctime(f'{OUTPUT_DIR}/{period}-{subperiod_id}.tsv')
+        dt = str(datetime.datetime.fromtimestamp(rev_time) \
+                 .astimezone(timezone('Australia/Melbourne'))).split('.')[0]
+        return dt
+
+    def __date_updated_sort_key(self, x):
+        """
+        Sort so that the most recent dates come first,
+        then sort by state, datatype and name
+        """
+        def sortable_date(i):
+            dd, mm, yyyy = i.split('/')
+            return (
+                str(9999-int(yyyy)) + '_' +
+                str(99-int(mm)) + '_' +
+                str(99-int(dd))
+            )
+
+        return (
+            sortable_date(x['date_updated']),
+            x['state_name'],
+            x['datatype'],
+            x['name']
+        )
+
+    def __generic_sort_key(self, x):
+        """
+        Sort only by state, datatype and name, ignoring date
+        """
+        print(x)
+        return (
+            x['state_name'],
+            x['datatype'],
+            x['name']
+        )
 
     #=============================================================#
     #                            Index                            #
@@ -178,8 +211,128 @@ class App(object):
         )
 
     @cherrypy.expose
-    def get_output_table(self):
-        pass
+    def statistics(self, rev_date, rev_subid):
+        """
+        TODO:
+        1. Get a list of unique dates for which datatype exists
+        2. Select the dicts for each of the values(s)
+           by the state_name, datatype
+        3.
+        """
+        datapoints = self.__read_csv(rev_date, rev_subid)
+        statistics_datapoints = self.__get_combined_values(
+            datapoints,
+            (
+                ('DT_CASES', 'None'),
+                ('DT_NEW_CASES', 'None'),
+                ('DT_PATIENT_STATUS', 'Deaths'),
+                #('DT_PATIENT_STATUS', 'New Deaths'),
+                ('DT_PATIENT_STATUS', 'Recovered'),
+                #('DT_PATIENT_STATUS', 'New Recovered'),
+                ('DT_CASES_TESTED', 'None'),
+                ('DT_SOURCE_OF_INFECTION', 'Locally acquired - contact of a confirmed case'),
+                ('DT_SOURCE_OF_INFECTION', 'Locally acquired - contact not identified'),
+                ('DT_SOURCE_OF_INFECTION', 'Interstate acquired'),
+                ('DT_PATIENT_STATUS', 'Hospitalized'),
+                ('DT_PATIENT_STATUS', 'ICU'),
+            )
+        )
+        return env.get_template('revision/statistics.html').render(
+            int=int,
+            revision_time_string=self.__get_revision_time_string(
+                rev_date, rev_subid
+            ),
+            statistics_datapoints=statistics_datapoints
+        )
+
+    def __get_combined_values(self, datapoints, filters, from_date=None):
+        """
+        Returns as a combined dict,
+        e.g. if filters (a list of ((datatype, name/None), ...) is (
+            ("DT_PATIENT_STATUS", "Recovered"),
+            ("DT_PATIENT_STATUS", "ICU")
+        )
+        it will output as [{
+            'date_updated': ...,
+            'DT_PATIENT_STATUS (Recovered)': ...,
+            'DT_PATIENT_STATUS (ICU)': ...
+        }, ...]
+        """
+        def to_datetime(dt):
+            return datetime.datetime.strptime(dt, '%d/%m/%Y')
+
+        combined = {}
+        for datatype, name in filters:
+            for datapoint in self.__get_combined_value(datapoints, datatype, name):
+                i_combined = combined.setdefault(datapoint['state_name'], {})
+
+                if (
+                    not 'date_updated' in i_combined or
+                    to_datetime(datapoint['date_updated']) <
+                        to_datetime(i_combined['date_updated'])
+                ):
+                    # Use the least recent date
+                    i_combined['date_updated'] = datapoint['date_updated']
+                    i_combined['date_today'] = datetime.datetime.now() \
+                        .strftime('%d/%m/%Y')
+
+                k = (
+                    f'{datatype} ({datapoint["name"]})'
+                    if datapoint["name"] != 'None'
+                    else datatype
+                )
+
+                i_combined['state_name'] = datapoint['state_name']
+                i_combined[k] = datapoint['value']
+                i_combined[f'{k} date_updated'] = datapoint['date_updated']
+                i_combined[f'{k} source_url'] = datapoint['source_url']
+
+        out = []
+        for i_combined in combined.values():
+            out.append(i_combined)
+        return out
+
+    def __get_combined_value(self, datapoints, datatype, name=None, from_date=None):
+        """
+        Filter `datapoints` to have only `datatype` (e.g. "DT_PATIENT_STATUS"),
+        and optionally only have `name` (e.g. "Recovered" or "None" as a string)
+
+        Returns only the most recent value (optionally from `from_date`)
+        """
+
+        def date_greater_than(x, y):
+            dd1, mm1, yyyy1 = x.split('/')
+            x = (int(yyyy1), int(mm1), int(dd1))
+
+            dd2, mm2, yyyy2 = y.split('/')
+            y = (int(yyyy2), int(mm2), int(dd2))
+
+            return x >= y
+
+        r = {}
+        for datapoint in datapoints:
+            if datapoint['datatype'] != datatype:
+                continue
+            elif name is not None and datapoint['name'] != name:
+                continue
+            elif from_date is not None and not date_greater_than(
+                from_date, datapoint['date_updated']
+            ):
+                continue
+
+            # Note we're restricting to only `datatype` already,
+            # so no need to include it in the key
+            unique_k = (
+                datapoint['state_name'],
+                datapoint['name']
+            )
+            if unique_k in r:
+                continue
+            r[unique_k] = datapoint
+
+        r = list(r.values())
+        r.sort(key=self.__generic_sort_key)
+        return r
 
 
 if __name__ == '__main__':
