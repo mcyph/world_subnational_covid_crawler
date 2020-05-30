f1222c58488e9f1b9d85922e8c90acc9cb9d9c15
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-05-30 12:29:35 +1000

    started adding mappings from English names to iso3166_1 and iso3166_2

--- state_news_releases/DataPoint.py
+++ state_news_releases/DataPoint.py
@@ -1,5 +1,9 @@
+import unidecode
 from collections import namedtuple
-from covid_19_au_grab.state_news_releases.constants import SCHEMA_ADMIN_1
+
+from covid_19_au_grab.state_news_releases.constants import SCHEMA_ADMIN_0, SCHEMA_ADMIN_1
+from covid_19_au_grab.other_data.iso_3166_1 import iso_3166_data
+from covid_19_au_grab.other_data.iso_3166_2 import iso_3166_2_data
 
 
 def DataPoint(region_parent=None,
@@ -26,6 +30,26 @@ def DataPoint(region_parent=None,
     assert value is not None
     text_match = text_match or ''
 
+    if region_schema == SCHEMA_ADMIN_1 and region_parent and region_child:
+        # Convert names to ISO codes
+        try:
+            item = iso_3166_2_data.get_data_item_by_name(
+                unidecode.unidecode(region_child), region_parent
+            )
+            region_parent = item.country_code
+            region_child = item.code
+        except KeyError:
+            pass
+    elif region_schema == SCHEMA_ADMIN_0 and region_child:
+        assert not region_parent, region_parent
+        try:
+            item = iso_3166_data.get_data_item_by_name(
+                region_child
+            )
+            region_child = item.iso3166.a2
+        except KeyError:
+            pass
+
     return _DataPoint(
         region_parent, region_schema, datatype,
         agerange, region_child, value,
@@ -46,3 +70,17 @@ _DataPoint = namedtuple('DataPoint', [
     # The text which matched this (the x,y range and text itself)
     'text_match'
 ])
+
+
+class _DataPointProcessor:
+    def process(self):
+        pass
+
+    def convert_admin_0(self):
+        pass
+
+    def convert_admin_1(self):
+        pass
+
+
+
